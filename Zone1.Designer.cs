45769d5df4601be0535ccec1192c81ceeeaf39a6
QianbiaoZhao <69677477+zhaoqianbiao@users.noreply.github.com>
2024-12-30 20:58:09 +0800

    Add files via upload

---
+++ Zone1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WordAddIns
+{
+    partial class Zone1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Zone1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Zone1));
+            this.QbTools = this.Factory.CreateRibbonTab();
+            this.Group1 = this.Factory.CreateRibbonGroup();
+            this.OpenInFolder = this.Factory.CreateRibbonButton();
+            this.QbTools.SuspendLayout();
+            this.Group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // QbTools
+            // 
+            this.QbTools.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.QbTools.ControlId.OfficeId = "TabHome";
+            this.QbTools.Groups.Add(this.Group1);
+            this.QbTools.Label = "TabHome";
+            this.QbTools.Name = "QbTools";
+            // 
+            // Group1
+            // 
+            this.Group1.Items.Add(this.OpenInFolder);
+            this.Group1.Label = "Group1";
+            this.Group1.Name = "Group1";
+            // 
+            // OpenInFolder
+            // 
+            this.OpenInFolder.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.OpenInFolder.Image = ((System.Drawing.Image)(resources.GetObject("OpenInFolder.Image")));
+            this.OpenInFolder.Label = "OpenInFolder";
+            this.OpenInFolder.Name = "OpenInFolder";
+            this.OpenInFolder.ShowImage = true;
+            this.OpenInFolder.SuperTip = "OpenDocxInFolder在文件夹中显示";
+            this.OpenInFolder.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.OpenInFolder_Click);
+            // 
+            // Zone1
+            // 
+            this.Name = "Zone1";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.QbTools);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Zone1_Load);
+            this.QbTools.ResumeLayout(false);
+            this.QbTools.PerformLayout();
+            this.Group1.ResumeLayout(false);
+            this.Group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab QbTools;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup Group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton OpenInFolder;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Zone1 Zone1
+        {
+            get { return this.GetRibbon<Zone1>(); }
+        }
+    }
+}
---
+++ Zone1.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WordAddIns
+{
+    public partial class Zone1
+    {
+        private void Zone1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        public static string ConvertOneDriveUrlToLocalPath(string url)
+        {
+            // 替换为你的 OneDrive 本地文件夹路径
+            string onedriveLocalPath = Environment.ExpandEnvironmentVariables(@"C:\Users\qianb\OneDrive");
+            try
+            {
+                // 提取 URL 中的相对路径
+                Uri uri = new Uri(url);
+                string relativePath = uri.AbsolutePath.Substring(uri.AbsolutePath.IndexOf("/d.docs.live.net/") + "/d.docs.live.net/".Length);
+                relativePath = relativePath.Substring(relativePath.IndexOf("/") + 1);
+
+                // 解码 URL 中的特殊字符
+                relativePath = Uri.UnescapeDataString(relativePath);
+
+                // 构建本地路径
+                string localPath = Path.Combine(onedriveLocalPath, relativePath.Replace("/", Path.DirectorySeparatorChar.ToString()));
+
+                // 规范化路径以使用正确的分隔符
+                localPath = Path.GetFullPath(localPath);  
+                return localPath;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: Unable to parse OneDrive URL. {ex.Message}");
+                return null;
+            }
+        }
+
+        private string GetActiveDocumentPath()
+        {
+            // 获取当前活动文档的完整路径
+            var activeDocument = Globals.ThisAddIn.Application.ActiveDocument;
+            if (activeDocument != null)
+            {
+                return activeDocument.FullName;
+            }
+            return null;
+        }
+
+        private void OpenInFolder_Click(object sender, RibbonControlEventArgs e)
+        {
+            // 获取当前活动文档的路径
+            string activeDocumentPath = GetActiveDocumentPath();
+
+            // 判断是否需要对OneDrive URL 进行转换
+            if (activeDocumentPath.Contains("d.docs.live.net"))
+            {
+                activeDocumentPath = ConvertOneDriveUrlToLocalPath(activeDocumentPath);
+            }
+
+
+            if (!string.IsNullOrEmpty(activeDocumentPath))
+            {
+                // 获取文件所在的目录
+                string directoryPath = Path.GetDirectoryName(activeDocumentPath);
+
+                if (Directory.Exists(directoryPath))
+                {
+                    // 打开文件所在的目录
+                    Process.Start("explorer.exe", $"/select,\"{activeDocumentPath}\"");
+
+                    //System.Diagnostics.Process.Start("explorer.exe", "/select," + activeDocumentPath);
+
+
+                    /*var startInfo = new ProcessStartInfo
+                    {
+                        FileName = "explorer.exe",
+                        Arguments = $"/select,\"{activeDocumentPath}\"",
+                        UseShellExecute = true,
+                        WindowStyle = ProcessWindowStyle.Normal
+                    };
+
+                    Process.Start(startInfo);*/
+                }
+                else
+                {
+                    MessageBox.Show("目录不存在。");
+                }
+            }
+            else
+            {
+                MessageBox.Show("未找到活动文档。");
+            }
+        }
+    }
+}
